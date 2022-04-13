ea0304ad8b412d8d15b597382952e83b904851f6
RyeoIn <94438120+RyeoIn@users.noreply.github.com>
2022-04-14 04:08:31 +0900

    메인화면 기능 업데이트_2차
    -고친 것
    첫째로, 두 번 뒤로 가기를 눌러야 앱이 종료되도록 했다. 메인 액티비티 클래스에 지역 변수 count를 0으로 초기화해서, 뒤로 가기를 눌렀을 경우 count를 검사한다. 
        1) 만약 count가 0일 경우, count에 1을 더하고 한 번 더 눌러야 앱이 종료된다고 토스트를 띄운다.
        2) 만약 count가 1일 경우, finish( )로 앱을 종료한다.
    -고칠 것
    첫째로, 설명이랑 합치기 위해서는 인텐트를 사용해야 한다. 설명도 액티비티이기 때문에. 이때, 플래그 single_top, new_task, clear_top 설정하는 거 잊지 말기. 이걸 해야 어느 메뉴를 갔다 오든 메인 화면에서 정상적으로 앱을 종료할 수 있다.

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,17 @@
 package org.agsj;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         Button button2 = findViewById(R.id.button3);
         Button button3 = findViewById(R.id.settings);
         Button button4 = findViewById(R.id.info);
+        Button button5 = findViewById(R.id.infobutton);
+        LinearLayout infolayout = findViewById(R.id.infolayout);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +52,29 @@ public class MainActivity extends AppCompatActivity {
                 showToast("설명 창을 띄웁니다.");
             }
         });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                infolayout.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            if(count == 1) {
+                finish();
+            } else {
+                showToast("한 번 더 뒤로가기를 누를 경우 앱이 종료됩니다.");
+                count++;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     void showToast(String data) {
